c12893047a346e9bf2d2ae09e54a69f936c65eaf
hjcalvo <ingecalvo@gmail.com>
2017-09-06 22:07:38 -0500

    archivos copiados del repositorio privado msi-manager-style

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function($) {
+	$(function() {
+		$('.button-collapse').sideNav({
+			menuWidth: 200,
+			edge: 'left',
+			closeOnClick: true,
+			draggable: true,
+			onOpen: function(el) {},
+			onClose: function(el) {},
+		});
+	});
+})(jQuery); 
